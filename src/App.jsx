d2dcdb3230dbf3c0bc05f3b6a6b9db23185e6832
Vicente Duch <vhduch@gmail.com>
2024-10-24 19:44:23 +0200

    lab done Vicente Duch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
@@ -12,11 +12,12 @@ function App() {
       <Navbar />
 
       <div className="pages">
-        <HomePage />
+        <Routes>
+          <Route path="/" element={< HomePage />} />
+          <Route path="/students/:studentId" element={< StudentDetailsPage />} />
+          <Route path="/profile" element={< UserProfilePage />} />
+        </Routes>
 
-        <StudentDetailsPage />
-
-        <UserProfilePage />
       </div>
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "./../assets/logo-ironhack-blue.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -8,25 +9,29 @@ function Navbar() {
 
           {/* Home Button (Logo) */}
           <button className="flex items-center text-l py-1">
-            <img src={logo} alt="Logo" className="h-8 w-auto" />
+            <Link to="/">
+              <img src={logo} alt="Logo" className="h-8 w-auto" />
+            </Link>
           </button>
         </div>
 
         <div className="flex justify-center w-1/2">
-        <span className="text-xl">
+          <span className="text-xl">
             Cohort Tools
           </span>
         </div>
 
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
-          <button className="flex items-center text-l py-1">
-            <img
-              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
-              alt="Logo"
-              className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
-            />
-          </button>
+          <Link to="/profile">
+            <button className="flex items-center text-l py-1">
+              <img
+                src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
+                alt="Logo"
+                className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
+              />
+            </button>
+          </Link>
 
         </div>
       </div>
